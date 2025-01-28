bbdc17bc7708e7e0a52907fa45f705173af9aa7a
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-01-28 19:55:35 +0000

    Premier commit - Ajout du projet

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,9 +6,9 @@ const Card = () => {
     <div className='flex flex-col p-8'>
         <img className='max-h-[260px] max-w-[240px] border rounded' 
         src={Glasses} 
-        alt="Glasses"/>
+        alt="Glasses"/> 
     </div>
   )
 }
 
-export default Card
+export default Card;

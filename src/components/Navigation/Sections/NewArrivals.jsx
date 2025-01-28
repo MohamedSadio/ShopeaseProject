6bfcd078d320190643d68aadce8dc3f48689861b
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-01-28 19:55:35 +0000

    Premier commit - Ajout du projet

---
+++ src/components/Navigation/Sections/NewArrivals.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeading from '../../SectionsHeading/SectionHeading'
+import Card from '../../Card/Card'
+
+const NewArrivals = () => {
+  return (
+    <>
+    <SectionHeading title={'New Arrivals'}/>
+    <Card/>
+    </>
+  )
+}
+
+export default NewArrivals
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    fill: true, // Active explicitement 'fill' si désactivé
+  },
+};

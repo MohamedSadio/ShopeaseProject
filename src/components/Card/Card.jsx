bd4c86cfbed457e0c24ef47012194183882fe5c8
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-02-03 19:56:59 +0000

    Premier commit - Ajout du projet

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Glasses from '../../assets/images/Glasses.jpg'
+
+const Card = () => {
+  return (
+    <div className='flex flex-col p-8'>
+        <img className='max-h-[260px] max-w-[240px] border rounded' 
+        src={Glasses} 
+        alt="Glasses"/>
+    </div>
+  )
+}
+
+export default Card

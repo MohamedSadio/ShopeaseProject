e1f7c8e1eee43fd70381af39984e08edb5cc65da
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-02-27 19:15:18 +0000

    m

--- src/components/Navigation/Sections/Categories/Category.jsx
+++ src/components/Navigation/Sections/Categories/Category.jsx
@@ -6,7 +6,7 @@ const Category = ({title,data}) => {
   return (
     <>
     <SectionHeading title={title}/>
-    <div div className='flex px-8'>
+    <div className='flex px-8'>
     {data && data?.map((item,index)=>{
         return (
             <Card key={index} title={item?.title} description={item?.description} imagePath={item?.image}

5b3b23e7d573ca05cdc3c5242ca2dc20e6864c70
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-03-18 22:22:25 +0000

    m

--- src/pages/ProductDetailPage/ProductDetails.jsx
+++ src/pages/ProductDetailPage/ProductDetails.jsx
@@ -1,28 +1,59 @@
-import React from 'react'
-import { useLoaderData, useParams } from 'react-router-dom'
+import React, { useEffect, useMemo, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
+import content from '../../data/content.json'
+
+const categories = content?.categories;
 
 const ProductDetails = () => {
     const { product } = useLoaderData();
+    const [image,setImage] = useState(product?.images[0] ?? product?.thumbnail);
+    const [breadCrumbLinks,setBreadCrumbLink]=useState([]);
+
+    const productCategory = useMemo(()=>{
+        return categories?.find((category)=> category?.id===product?.category_id)
+    },[product]);
+    useEffect(()=>{
+        setBreadCrumbLink([]);
+        const arrayLinks = [{title:'Shop',path:'/'},{
+            title:productCategory?.name,
+            path:productCategory?.path
+        }]
+        const productType = productCategory?.types?.find((item)=>item?.type_id === product?.type_id)
+        if(productType){
+            arrayLinks?.push({
+                title:productType?.name
+            })
+        }
+        setBreadCrumbLink(arrayLinks);
+    },[productCategory,product])
 
   return (
-    <div className='flex flex-col sm:flex-row p-10 '>
-        <div className='w-100% lg:w-50% md:w-40% sm:w-40%'>
+    <div className='flex flex-col md:flex-row px-10'>
+        <div className='w-100% lg:w-50% md:w-40%'>
             {/*image*/}
-            <div className='flex flex-col sm:flex-row'>
-                <div className='w-100% sm:w-30% justify-center h-320px'>
+            <div className='flex flex-col md:flex-row'>
+                <div className='w-100% sm:w-20% justify-center h-40px md:h-420px'>
                     {/*Stack images */}
-                    <div className='flex flex-col justify-center h-full'>
-                        <p>hello </p>
+                    <div className='flex flex-row md:flex-col justify-center h-full'>
+                        {product?.images?.map((item,index)=>(
+                            <button onClick={()=>setImage(item)} className='rounded-lg w-fit p-2 mb-2'><img src={item} alt={'sample-'+index} 
+                            className='h-60px w-60px bg-cover bg-center rounded-lg hover:scale-110 transform'/></button>
+                        ))}
                     </div>
                 </div>
-                <div className='w-70%'>
+                <div className='w-full md:w-80% flex justify-center md:pt-0 pt-10'>
                     {/* */}
-                    <p>hello image</p>
+                    <img src={image} 
+                    className='h-520px w-full
+                    border rounded-xl cursor-pointer object-cover block' 
+                    alt={product?.title}/>
                 </div>
             </div>
         </div>
-        <div className='w-60%'>
+        <div className='w-60% px-10'>
             {/*details*/}
+            <Breadcrumb links={breadCrumbLinks}/>
         </div>
     </div>
   )

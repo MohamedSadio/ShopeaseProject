e35dfde914d4f0b76eb1537eb9a9fca4b24b96b9
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-08-15 00:53:13 +0000

    m

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect,useRef  } from 'react';
 import HeroSection from './components/HeroSection/HeroSection';
 import NewArrivals from './components/Navigation/Sections/NewArrivals';
 import Category from './components/Navigation/Sections/Categories/Category';
 import content from './data/content.json';
 import Footer from './components/Navigation/Footer/Footer';
+import { fetchCategories } from './api/fetchCategories';
 
 const Shop = () => {
+  const hasFetched = useRef(false);
+  useEffect(()=>{
+    if (hasFetched.current) {
+      console.log(" Categories already fetched, skipping double call");
+      return;
+    }
+
+    hasFetched.current = true;
+
+    fetchCategories().then(res =>{
+       console.log("Categories",res); 
+    }).catch(err=>{
+      console.log("Error",err);
+    })
+  },[]);
+
   return (
     <>
       <HeroSection />
       <NewArrivals />
-      {/* Utilise content.pages.shop.sections au lieu de content.categories */}
       {content?.pages?.shop?.sections && content?.pages?.shop?.sections?.map((section, index) => (
         <Category key={section?.title + index} section={section} />
       ))}
